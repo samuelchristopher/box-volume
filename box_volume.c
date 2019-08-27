f4b78343921471430f350f872a858a9d2d966061
Samuel Christopher <thesamchris@gmail.com>
2019-08-27 17:53:33 +0800

    Finished

---
+++ box_volume.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+int main(void) {
+	int length, width, height, volume;
+
+	printf("Enter length: ");
+	scanf("%d", &length);
+
+	printf("Enter width: ");
+	scanf("%d", &width);
+
+	printf("Enter height: ");
+	scanf("%d", &height);
+
+	volume = length*width*height;
+
+	printf("Answer is %d \n", volume);
+
+	return 0;
+
+}
